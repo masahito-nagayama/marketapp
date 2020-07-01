5a4634fc5209da916697e84627752125aa541e64
EC2 Default User <ec2-user@ip-172-31-42-34.ec2.internal>
2020-07-02 00:49:10 +0900

    add-sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'users/top'
-
+  get '/users/sign_up' => 'users#sign_up' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
